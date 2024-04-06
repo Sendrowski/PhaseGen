cd6f9caf6d749b403f5f7821e120d13c7f1666a4
Sendrowski <j.sendrowski18@gmail.com>
2024-04-06 19:38:50 +0200

    Prepare for new release.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = 'PhaseGen'
 year = datetime.datetime.now().year
 copyright = f'{year}, Janek Sendrowski'
 author = 'Janek Sendrowski'
-release = '0.0.4-beta'
+release = '0.0.5-beta'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
