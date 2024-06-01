5ee29b054ca1d19c5889395039a935b2b13fca28
Sendrowski <j.sendrowski18@gmail.com>
2024-06-01 08:25:46 +0200

    Remove unneeded tests.

--- testing/test_comparison.py
+++ testing/test_comparison.py
@@ -4,8 +4,6 @@ Test comparison.
 
 from unittest import TestCase
 
-import pytest
-
 from phasegen.comparison import Comparison
 
 
@@ -49,39 +47,3 @@ class ComparisonTestCase(TestCase):
             )
 
             c.compare(title="mutation_config_test_size")
-
-    @staticmethod
-    @pytest.mark.skip("not needed")
-    def test_3_epoch_beta_migration_disparate_migration_sizes_2_each_n_6_early_end_time():
-        """
-        Test simple comparison.
-        """
-        c = Comparison.from_yaml(
-            "../resources/configs/3_epoch_beta_migration_disparate_migration_sizes_2_each_n_6_early_end_time.yaml")
-
-        c.parallelize = False
-        c.num_replicates = 10000
-
-        # touch msprime stats to cache them
-        c.ms.touch()
-
-        # drop simulated data
-        c.ms.drop()
-
-        c.compare(title="3_epoch_beta_migration_disparate_migration_sizes_2_each_n_6_early_end_time")
-
-    @staticmethod
-    @pytest.mark.skip(reason="takes too long, using cached scenarios instead")
-    def test_1_epoch_n_10():
-        """
-        Test simple comparison.
-        """
-        c = Comparison.from_yaml("../resources/configs/1_epoch_n_10.yaml")
-
-        # touch msprime stats to cache them
-        c.ms.touch()
-
-        # drop simulated data
-        c.ms.drop()
-
-        c.compare(title="1_epoch_n_10")
